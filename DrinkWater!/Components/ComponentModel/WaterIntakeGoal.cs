e7bac4bf88c38ddf1eefb3be21aa9bd55445d78a
Lucas Blomhäll <lucas.blomhall@gmail.com>
2023-11-25 16:30:13 +0100

    Made 3 pictures and goes through them with the button

---
+++ DrinkWater!/Components/ComponentModel/WaterIntakeGoal.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrinkWater.Components.ComponentModel
+{
+    public class WaterIntakeGoal
+    {
+        [Required]
+        [Range(1, 20, ErrorMessage = "Goal should be between 1 and 20 glasses.")]
+        public int Goal { get; set; } = 8; // Default value
+    }
+}
