9e34ac251027b9cd049cfa463de5f837aacbea58
Lucas Blomhäll <lucas.blomhall@gmail.com>
2023-11-28 13:06:51 +0100

    Adds news bottles when pressing button

---
+++ DrinkWater!/Components/ComponentModel/ImageListBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DrinkWater.Components.ComponentModel
+{
+    public class ImageListBase : ComponentBase
+    {
+        public IEnumerable<ImageListBase> Images { get; set; }
+    }
+}
